f8ed497d0d95112a8f73f96e9aa34283c7ae5b91
DK\AMCH <amch@sitecore.net>
2016-12-12 09:54:35 +0200

    commit patch 67014

---
+++ src/Sitecore.Support.67014/AddCustomMetadataProvider.cs
@@ -0,0 +1,37 @@
+﻿using Sitecore;
+using Sitecore.Abstractions;
+using Sitecore.Diagnostics;
+using Sitecore.Forms.Mvc;
+using Sitecore.Forms.Mvc.Controllers.ModelBinders;
+using Sitecore.Forms.Mvc.Metadata;
+using Sitecore.Forms.Mvc.ViewModels;
+using Sitecore.Pipelines;
+using Sitecore.WFFM.Abstractions.Shared;
+using System;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.Forms.Mvc.Pipelines.Initialize
+{
+    public class AddCustomMetadataProvider
+    {
+        private readonly ICorePipeline corePipeline;
+        private readonly IPerRequestStorage perRequestStorage;
+
+        public AddCustomMetadataProvider(IPerRequestStorage perRequestStorage, ICorePipeline corePipeline)
+        {
+            Assert.IsNotNull(perRequestStorage, "perRequestStorage");
+            Assert.IsNotNull(corePipeline, "corePipeline");
+            this.perRequestStorage = perRequestStorage;
+            this.corePipeline = corePipeline;
+        }
+
+        [UsedImplicitly]
+        public virtual void Process(PipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            ModelMetadataProviders.Current = new PipelineBasedDataAnnotationsModelMetadataProvider(this.perRequestStorage, this.corePipeline);
+            ModelBinders.Binders.Add(typeof(SectionViewModel), new Sitecore.Support.Forms.Mvc.Controllers.ModelBinders.SectionModelBinder());
+            ModelBinders.Binders.Add(typeof(FieldViewModel), new FieldModelBinder());
+        }
+    }
+}
---
+++ src/Sitecore.Support.67014/SectionModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Forms.Mvc.Data.Wrappers;
+using Sitecore.Forms.Mvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.Forms.Mvc.Controllers.ModelBinders
+{
+    public class SectionModelBinder : Sitecore.Forms.Mvc.Controllers.ModelBinders.SectionModelBinder
+    {
+        protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
+        {
+            FormViewModel formViewModel = this.GetFormViewModel(controllerContext);
+            if (formViewModel != null)
+            {
+                int num = int.Parse(bindingContext.ModelName.Substring(bindingContext.ModelName.IndexOf('[') + 1, (bindingContext.ModelName.IndexOf(']') - bindingContext.ModelName.IndexOf('[')) - 1));
+                return formViewModel.Sections[num];
+            }
+            return base.CreateModel(controllerContext, bindingContext, modelType);
+        }
+    }
+}
