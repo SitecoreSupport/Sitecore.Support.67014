70e34bb19ebd1f7979d464f98cb6321f118e75ad
DK\dmsa <dmsa@sitecore.net>
2017-08-16 09:33:09 +0300

    Fix for issue #67014 --- Section is validated against another section if there are more than 10 sections in form

---
+++ src/Sitecore.Support.67014/Forms/Mvc/Controllers/ModelBinders/SectionModelBinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Sitecore.Forms.Mvc.ViewModels;
+using System;
+
+namespace Sitecore.Support.Forms.Mvc.Controllers.ModelBinders
+{
+    [ModelBinder(typeof(SectionModelBinder))]
+    public class SectionModelBinder : Sitecore.Forms.Mvc.Controllers.ModelBinders.SectionModelBinder
+    {
+        protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
+        {
+            FormViewModel formViewModel = this.GetFormViewModel(controllerContext);
+            if (formViewModel != null)
+            {
+                int num = int.Parse(bindingContext.ModelName.Substring(bindingContext.ModelName.IndexOf('[') + 1, (bindingContext.ModelName.IndexOf(']') - bindingContext.ModelName.IndexOf('[')) - 1));
+                return formViewModel.Sections[num];
+            }
+            return base.CreateModel(controllerContext, bindingContext, modelType);
+
+        }
+    }
+}
---
+++ src/Sitecore.Support.67014/Forms/Mvc/Pipelines/Initialize/AddCustomMetadataProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Sitecore;
+using Sitecore.Abstractions;
+using Sitecore.Diagnostics;
+using Sitecore.Forms.Mvc.Controllers.ModelBinders;
+using Sitecore.Forms.Mvc.Metadata;
+using Sitecore.Forms.Mvc.ViewModels;
+using Sitecore.Pipelines;
+using Sitecore.WFFM.Abstractions.Shared;
+using SupportSectionModelBinder = Sitecore.Support.Forms.Mvc.Controllers.ModelBinders.SectionModelBinder;
+
+namespace Sitecore.Support.Forms.Mvc.Pipelines.Initialize
+{
+    public class AddCustomMetadataProvider
+    {
+        private readonly ICorePipeline corePipeline;
+        private readonly IPerRequestStorage perRequestStorage;
+
+        public AddCustomMetadataProvider(IPerRequestStorage perRequestStorage, ICorePipeline corePipeline)
+        {
+            Assert.IsNotNull(perRequestStorage, "perRequestStorage");
+            Assert.IsNotNull(corePipeline, "corePipeline");
+            this.perRequestStorage = perRequestStorage;
+            this.corePipeline = corePipeline;
+        }
+
+        [UsedImplicitly]
+        public virtual void Process(PipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            ModelMetadataProviders.Current = new PipelineBasedDataAnnotationsModelMetadataProvider(this.perRequestStorage, this.corePipeline);
+            ModelBinders.Binders.Add(typeof(SectionViewModel), new SupportSectionModelBinder());
+            ModelBinders.Binders.Add(typeof(FieldViewModel), new FieldModelBinder());
+        }
+    }
+}
